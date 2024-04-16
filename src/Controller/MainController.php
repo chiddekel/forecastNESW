9141ec18a2542f986ef285cc52870262cdaf7294
bdelium <105479256+bdelium@users.noreply.github.com>
2024-04-16 20:28:59 +0200

    Lesson #40.

---
+++ src/Controller/MainController.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class MainController extends AbstractController
+{
+
+    #[Route('/')]
+
+    public function main(): Response
+    {
+        return $this->render('main.html.twig');
+    }
+}
