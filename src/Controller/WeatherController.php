89b17eedda12dcea0015fa953d23b05224eb6bff
bdelium <105479256+bdelium@users.noreply.github.com>
2025-01-03 19:55:04 +0100

    Grouping and Prefixing route.

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -7,11 +7,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
+// #[Route('/weather')]
 class WeatherController extends AbstractController
 {
 
-    // #[Route('/weather/highlander-says/{threshold<\d+>?50}')]
+    // #[Route('/highlander-says/{threshold<\d+>?50}')]
     public function highlanderSays(int $threshold): Response
     {
         $draw = random_int(0, 100);
@@ -21,7 +21,7 @@ class WeatherController extends AbstractController
         ]);
     }
 
-    // #[Route('/weather/highlander-says/{guess}')]
+    // #[Route('/highlander-says/{guess}')]
     public function highlanderSaysGuess(string $guess): Response    {
        
         $forecast = "It's going to $guess :-)"
