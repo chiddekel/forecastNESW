ccca03d55ace4e11fc18807d6770c41b5c35bf2b
bdelium <105479256+bdelium@users.noreply.github.com>
2025-01-11 19:26:10 +0100

    Lesson 53 - Forecast by City and Country.

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -8,13 +8,20 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 
 
 #[Route('/weather')]
 class WeatherController extends AbstractController
 {
+
+    private HttpClientInterface $httpClient;
+
+    public function __construct(HttpClientInterface $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
 
     #[Route('/highlander-says/{threshold<\d+>?50}', host: 'api.localhost')]
     public function highlanderSaysApi(int $threshold): Response
@@ -45,6 +52,44 @@ class WeatherController extends AbstractController
         ;
         return $this->render('weather/highlander-says.html.twig', [
             'forecast' => $forecast,
+        ]);
+    }
+
+    // #[Route('/{country_code<AF|AX|AL|DZ|AS|AD|AO|AI|AQ|AG|AR|AM|AW|AU|AT|AZ|BS|BH|BD|BB|BY|BE|BZ|BJ|BM|BT|BO|BA|BW|BV|BR|IO|BN|BG|BF|BI|CV|KH|CM|CA|KY|CF|TD|CL|CN|CX|CC|CO|KM|CG|CD|CK|CR|CI|HR|CU|CY|CZ|DK|DJ|DM|DO|EC|EG|SV|GQ|ER|EE|ET|FK|FO|FJ|FI|FR|GF|PF|TF|GA|GM|GE|DE|GH|GI|GR|GL|GD|GP|GU|GT|GG|GN|GW|GY|HT|HM|VA|HN|HK|HU|IS|IN|ID|IR|IQ|IE|IM|IL|IT|JM|JP|JE|JO|KZ|KE|KI|KP|KR|KW|KG|LA|LV|LB|LS|LR|LY|LI|LT|LU|MO|MG|MW|MY|MV|ML|MT|MH|MQ|MR|MU|YT|MX|FM|MD|MC|MN|ME|MS|MA|MZ|MM|NA|NR|NP|NL|NC|NZ|NI|NE|NG|NU|NF|MP|NO|OM|PK|PW|PS|PA|PG|PY|PE|PH|PN|PL|PT|PR|QA|RE|RO|RU|RW|BL|SH|KN|LC|MF|PM|VC|WS|SM|ST|SA|SN|RS|SC|SL|SG|SX|SK|SI|SB|SO|ZA|GS|SS|ES|LK|SD|SR|SJ|SE|CH|SY|TW|TJ|TZ|TH|TL|TG|TK|TO|TT|TN|TR|TM|TC|TV|UG|UA|AE|GB|US|UM|UY|UZ|VU|VE|VN|VG|VI|WF|EH|YE|ZM|ZW>}/{city_name}')]
+    // public function forecast(string $country_code, string $city_name)   
+    // {
+    //     $validCities = ['Barcelona', 'Paris', 'Berlin'];
+    //     if (!in_array($city_name, $validCities)) {
+    //         return new Response('Wrong City!', Response::HTTP_BAD_REQUEST);
+    //     }
+
+    //     return $this->render('weather/highlander-forecast.html.twig', [
+    //         'country_code' => $country_code,
+    //         'city_name' => $city_name,
+    //     ]);
+    // }
+
+    #[Route('/{country_code<AF|AX|AL|DZ|AS|AD|AO|AI|AQ|AG|AR|AM|AW|AU|AT|AZ|BS|BH|BD|BB|BY|BE|BZ|BJ|BM|BT|BO|BA|BW|BV|BR|IO|BN|BG|BF|BI|CV|KH|CM|CA|KY|CF|TD|CL|CN|CX|CC|CO|KM|CG|CD|CK|CR|CI|HR|CU|CY|CZ|DK|DJ|DM|DO|EC|EG|SV|GQ|ER|EE|ET|FK|FO|FJ|FI|FR|GF|PF|TF|GA|GM|GE|DE|GH|GI|GR|GL|GD|GP|GU|GT|GG|GN|GW|GY|HT|HM|VA|HN|HK|HU|IS|IN|ID|IR|IQ|IE|IM|IL|IT|JM|JP|JE|JO|KZ|KE|KI|KP|KR|KW|KG|LA|LV|LB|LS|LR|LY|LI|LT|LU|MO|MG|MW|MY|MV|ML|MT|MH|MQ|MR|MU|YT|MX|FM|MD|MC|MN|ME|MS|MA|MZ|MM|NA|NR|NP|NL|NC|NZ|NI|NE|NG|NU|NF|MP|NO|OM|PK|PW|PS|PA|PG|PY|PE|PH|PN|PL|PT|PR|QA|RE|RO|RU|RW|BL|SH|KN|LC|MF|PM|VC|WS|SM|ST|SA|SN|RS|SC|SL|SG|SX|SK|SI|SB|SO|ZA|GS|SS|ES|LK|SD|SR|SJ|SE|CH|SY|TW|TJ|TZ|TH|TL|TG|TK|TO|TT|TN|TR|TM|TC|TV|UG|UA|AE|GB|US|UM|UY|UZ|VU|VE|VN|VG|VI|WF|EH|YE|ZM|ZW>}/{city_name}', name: 'forecast')]
+    public function forecast(string $country_code, string $city_name): Response
+    {
+        $nominatimUrl = 'https://nominatim.openstreetmap.org/search';
+        $response = $this->httpClient->request('GET', $nominatimUrl, [
+            'query' => [
+                'q' => $city_name,
+                'countrycodes' => strtolower($country_code),
+                'format' => 'json',
+                'limit' => 1,
+            ],
+        ]);
+
+        $data = $response->toArray();
+        if (empty($data)) {
+            return new Response('Wrong City!', Response::HTTP_BAD_REQUEST);
+        }
+
+        return $this->render('weather/highlander-forecast.html.twig', [
+            'country_code' => $country_code,
+            'city_name' => $city_name,
         ]);
     }
 
