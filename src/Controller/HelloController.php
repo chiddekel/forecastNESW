63adff10e5f25e9e3bd981f46292b9f0e9283e79
bdelium <105479256+bdelium@users.noreply.github.com>
2025-11-14 21:13:43 +0100

    Lekcja 55.

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+
+class HelloController extends AbstractController  {
+	#[Route('/hello')]
+        public function __invoke(): Response {
+		return new Response('<h1>Hello!</h1>');
+	}
+
+}
